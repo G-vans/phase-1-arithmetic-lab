1f22bba5db71a36f8348e54bb2104864b61713ac
G-vans <jevans.otieno@student.moringaschool.com>
2022-08-17 05:22:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 
 // Write your code here
+
+//Result 62
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+
+//Random Number greater than 0
+
+let random = (Math.floor(Math.random() * 10) + 1);
+
+Number.parseInt(random, 10);
+
+console.log(Number.isInteger(random));
+
+//Remainder of 4
+let num3 = 19;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+console.log(mod);
+
+//Max as 20
+
+let max = Math.max(2,4,6,8,10,12,14,16,18,20);
+
+console.log(max);
